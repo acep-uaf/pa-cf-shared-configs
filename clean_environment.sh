fa72f55dfcf8a9a8495b31a7455dd2cfc3dce24c
jody <thisjody@gmail.com>
2023-10-04 11:00:23 -0700

    setting debug messages

--- clean_environment.sh
+++ clean_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 echo "Enter the path to the .env file to use (e.g. ./myenvfile.env):"
 read ENV_FILE
@@ -12,9 +12,8 @@ echo "Loaded environment variables from $ENV_FILE"
 DEPLOY_SA_EMAIL="${DEPLOY_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 PRIVILEGED_SA_EMAIL="${PRIVILEGED_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 
-# Check and delete service accounts
+# Check if service accounts exist and delete them if they do
 echo "Checking and deleting service accounts..."
-
 if gcloud iam service-accounts describe $DEPLOY_SA_EMAIL &>/dev/null; then
     gcloud iam service-accounts delete $DEPLOY_SA_EMAIL --quiet
     echo "Deleted service account: $DEPLOY_SA_EMAIL"
@@ -32,30 +31,37 @@ fi
 # Check for leftover bindings and unbind if needed
 for ROLE in $DEPLOY_ROLE_NAME $PRIVILEGED_ROLE_NAME; do
     echo "Checking for bindings of $ROLE to service accounts..."
+    echo "DEBUG: Current ROLE being checked: $ROLE"
 
     # Get bindings for the role
     BINDINGS=$(gcloud projects get-iam-policy $PROJECT_ID \
         --flatten="bindings[].members[]" \
         --filter="bindings.role:$ROLE AND (bindings.members:$DEPLOY_SA_EMAIL OR bindings.members:$PRIVILEGED_SA_EMAIL)" \
         --format="value(bindings.members)")
+    
+    echo "DEBUG: Retrieved bindings: $BINDINGS"
 
-    # Unbind if service accounts are still bound to the role
+    # Unbind if the deleted service accounts are still bound to the role
     if [[ ! -z $BINDINGS ]]; then
         for MEMBER in $BINDINGS; do
-            if gcloud projects remove-iam-policy-binding $PROJECT_ID \
+            # Extract the actual email from the MEMBER
+            ACTUAL_EMAIL=$(echo $MEMBER | awk -F'\\?uid=' '{print $1}' | sed 's/deleted:serviceAccount://')
+            
+            if ! gcloud projects remove-iam-policy-binding $PROJECT_ID \
                 --role=$ROLE \
-                --member=$MEMBER &>/dev/null; then
-                echo "Removed binding of $MEMBER to $ROLE"
+                --member=serviceAccount:$ACTUAL_EMAIL; then
+                echo "Failed to remove binding of $ACTUAL_EMAIL to $ROLE."
             else
-                echo "Failed to remove binding of $MEMBER to $ROLE. It might not exist."
+                echo "Successfully removed binding of $ACTUAL_EMAIL to $ROLE."
             fi
         done
     else
         echo "No leftover bindings found for $ROLE."
     fi
+
 done
 
-# Delete secret
+# Delete secret if it exists
 echo "Deleting secret..."
 if gcloud secrets describe $SECRET_NAME &>/dev/null; then
     gcloud secrets delete $SECRET_NAME --quiet
