c2dbe3930f07ea3e19aab9d2b6103cde09da50c6
jody <thisjody@gmail.com>
2023-10-06 12:15:02 -0700

    remove the bindings first

--- clean_environment.sh
+++ clean_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -e
 
 echo "Enter the path to the .env file to use (e.g. ./myenvfile.env):"
 read ENV_FILE
@@ -12,40 +12,38 @@ echo "Loaded environment variables from $ENV_FILE"
 DEPLOY_SA_EMAIL="${DEPLOY_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 PRIVILEGED_SA_EMAIL="${PRIVILEGED_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
 
-# Check if service accounts exist and delete them if they do
-echo "Checking and deleting service accounts..."
-
-if gcloud iam service-accounts list --project=$PROJECT_ID | grep -q $DEPLOY_SA_EMAIL; then
-    gcloud iam service-accounts delete $DEPLOY_SA_EMAIL --quiet
-    echo "Deleted service account: $DEPLOY_SA_EMAIL"
-else
-    echo "Service account $DEPLOY_SA_EMAIL does not exist."
-fi
-
-if gcloud iam service-accounts list --project=$PROJECT_ID | grep -q $PRIVILEGED_SA_EMAIL; then
-    gcloud iam service-accounts delete $PRIVILEGED_SA_EMAIL --quiet
-    echo "Deleted service account: $PRIVILEGED_SA_EMAIL"
-else
-    echo "Service account $PRIVILEGED_SA_EMAIL does not exist."
-fi
-
 # Check for leftover bindings and unbind if needed
-for ROLE in $DEPLOY_ROLE_NAME $PRIVILEGED_ROLE_NAME; do
-    echo "Checking for bindings of $ROLE to service accounts..."
-    echo "DEBUG: Current ROLE being checked: $ROLE"
+unbind_role_from_service_account() {
+    local ROLE_NAME=$1
+    local SA_EMAIL=$2
+    local FULL_ROLE_NAME="projects/$PROJECT_ID/roles/$ROLE_NAME"
+    local BINDING_EXISTS=$(gcloud projects get-iam-policy "$PROJECT_ID" --flatten="bindings[].members" --format='table(bindings.role,bindings.members)' | grep "$SA_EMAIL" | grep "$FULL_ROLE_NAME")
+    
+    if echo "$BINDING_EXISTS" | grep -q "$SA_EMAIL"; then
+        gcloud projects remove-iam-policy-binding "$PROJECT_ID" --member="serviceAccount:$SA_EMAIL" --role="$FULL_ROLE_NAME"
+        echo "Removed service account binding for $SA_EMAIL."
+    fi
+}
 
-    # Get bindings for the role
-    BINDINGS_JSON=$(gcloud projects get-iam-policy $PROJECT_ID --format=json)
-    EMAIL_BINDINGS=$(echo "$BINDINGS_JSON" | jq -r --arg ROLE "$ROLE" --arg EMAIL1 "$DEPLOY_SA_EMAIL" --arg EMAIL2 "$PRIVILEGED_SA_EMAIL" '.bindings[] | select(.role == $ROLE and (.members[] == ("deleted:serviceAccount:" + $EMAIL1 + "?uid=" + .members[] | split("?uid=")[1] ) or (.members[] == ("deleted:serviceAccount:" + $EMAIL2 + "?uid=" + .members[] | split("?uid=")[1])) ) | .members[] | split("?uid=")[0] | ltrimstr("deleted:serviceAccount:")')
+echo "Checking and removing bindings from service accounts..."
+unbind_role_from_service_account "$DEPLOY_ROLE_NAME" "$DEPLOY_SA_EMAIL"
+unbind_role_from_service_account "$PRIVILEGED_ROLE_NAME" "$PRIVILEGED_SA_EMAIL"
 
-    for EMAIL in $EMAIL_BINDINGS; do
-        if ! gcloud projects remove-iam-policy-binding $PROJECT_ID --role=$ROLE --member=serviceAccount:$EMAIL; then
-            echo "Failed to remove binding of $EMAIL to $ROLE."
-        else
-            echo "Successfully removed binding of $EMAIL to $ROLE."
-        fi
-    done
-done
+# Check if service accounts exist and delete them if they do
+delete_service_account() {
+    local SA_EMAIL=$1
+
+    if gcloud iam service-accounts list --project=$PROJECT_ID | grep -q $SA_EMAIL; then
+        gcloud iam service-accounts delete $SA_EMAIL --quiet
+        echo "Deleted service account: $SA_EMAIL"
+    else
+        echo "Service account $SA_EMAIL does not exist."
+    fi
+}
+
+echo "Deleting service accounts..."
+delete_service_account "$DEPLOY_SA_EMAIL"
+delete_service_account "$PRIVILEGED_SA_EMAIL"
 
 # Delete secret if it exists
 echo "Deleting secret..."
